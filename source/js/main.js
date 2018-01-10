5704d804ced3d60c7ceb4ffb75dad4bcb1f5671d
Tran.Hai.Phong <tranhaiphong.6868@gmail.com>
2018-01-10 08:40:32 +0700

    EditSourceStandard

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,5 @@
 //=require jquery/dist/jquery.min.js
+//=require popper.js/dist/umd/popper.min.js
+//=require bootstrap/dist/js/bootstrap.min.js
+//=require _toggle-sidebars.js
+//=require _fixed-head-table.js
