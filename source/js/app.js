025f61f394cae9439440c9025cd04bea8f4bdf42
ha <pein1625@gmail.com>
2018-01-11 10:46:48 +0700

    GS01-Task-Create-Common-Header

---
+++ source/js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  var $menuBtn = $('.js-menu-btn');
+  var $navBlock = $('.js-nav-block');
+  var $searchBtn = $('.js-search-btn');
+  var $headerBlock = $('.js-header-block');
+  var $btnDropdown = $('.js-btn-dropdown');
+  var $menuDropdown = $('.js-menu-dropdown');
+
+  $menuBtn.on('click', function(){
+    if ($(window).outerWidth() >= 992) {
+      $navBlock.toggleClass('active');
+    }
+    else {
+      $navBlock.slideToggle(200);
+    }
+  });
+  $searchBtn.on('click', function(e){
+    $headerBlock.addClass('active');
+    e.stopPropagation();
+  });
+  $headerBlock.on('click', function(e){
+    e.stopPropagation();
+  });
+  $btnDropdown.on('click', function(e){
+    $(this).siblings().children('.js-menu-dropdown').slideUp(200);
+    $(this).children('.js-menu-dropdown').slideToggle(200);
+    e.stopPropagation();
+  });
+  $menuDropdown.on('click', function(e){
+    e.stopPropagation();
+  });
+  $('html, body').on('click', function(){
+    $headerBlock.removeClass('active');
+    $menuDropdown.slideUp(100);
+  });
+});
--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,4 @@
 //=require jquery/dist/jquery.min.js
+//=require popper.js/dist/umd/popper.js
+//=require bootstrap/dist/js/bootstrap.js
+//=require app.js
